8d2642c86b4b87c6ac003855f40445659776bfde
Jean Santos <jean.santos@zup.com.br>
2019-01-30 15:29:57 -0200

    Implement ifinit tableView. (reloadData)

--- CompletionProject/CompletionProject/Controller/ViewController.swift
+++ CompletionProject/CompletionProject/Controller/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
+  // MARK: - Properties
+  var elements = [1,2,3,4,5,6]
+  
   // MARK: - Outlets
   @IBOutlet weak var tableView: UITableView!
   
@@ -19,9 +22,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     tableView.delegate = self
   }
   
-  // MARK: Extensions
+  // MARK: - Internal Methods
+  func loadDada() {
+    let newListElements = [7,8,9,10,11,12,13]
+    elements.append(contentsOf: newListElements)
+    
+    // reload data in tableview
+    tableView.reloadData()
+  }
+  
+  // MARK: TableView DataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return elements.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -32,4 +44,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     return cell
   }
   
+  // MARK: TableView Delegate
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let lastItem = elements.count - 1
+    if indexPath.row == lastItem {
+      loadDada()
+    }
+  }
 }
