33919202420a05a0348576524f2a93e1e2fbc16c
Jean Santos <jean.santos@zup.com.br>
2019-02-06 11:07:19 -0200

    * Modified how APP downloads images; * Modified UIImage+dowload with objc.

--- CompletionProject/CompletionProject/Controller/ViewController.swift
+++ CompletionProject/CompletionProject/Controller/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
   // MARK: - Properties
   var elements = [AppInfo]()
+  var referenceDatabase: DatabaseReference!
   
   // MARK: - Outlets
   @IBOutlet weak var tableView: UITableView!
@@ -19,24 +21,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let appInfo = AppInfo(name: "Soda Dungeon", price: 99, imageURL: "https://presskits.armorgames.com/game/soda-dungeon/images/SodaDungeonArt.png")
-    for _ in 0..<10 {
-      elements.append(appInfo)
-    }
+    referenceDatabase = Database.database().reference()
+    
+    // Get database elements
+    uploadData()
     
     tableView.dataSource = self
     tableView.delegate = self
   }
   
   // MARK: - Internal Methods
-  func loadDada() {
-    let appInfo = AppInfo(name: "Novos Dados", price: 199, imageURL: "https://presskits.armorgames.com/game/soda-dungeon/images/SodaDungeonArt.png")
-    for _ in 0..<10 {
-      elements.append(appInfo)
-    }
+  func uploadData() {
+    let folderApps = referenceDatabase.child("apps")
     
-    // reload data in tableview
-    tableView.reloadData()
+    folderApps.queryOrderedByKey()
+    folderApps.queryStarting(atValue: self.elements.count)
+    folderApps.queryEnding(atValue: self.elements.count + 4)
+    
+    folderApps.observeSingleEvent(of: .value) { (snapShot) in
+      for item in snapShot.children {
+        let appInfo = AppInfo(snapShot: item as! DataSnapshot)
+        self.elements.append(appInfo)
+      }
+      self.tableView.reloadData()
+    }
   }
   
   func formatPriceInCents(price: Int) -> String {
@@ -60,7 +68,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     let appInfo = elements[indexPath.row]
     cell.name.text = appInfo.name
     cell.price.text = formatPriceInCents(price: appInfo.price)
-    cell.imageGame.downloaded(from: appInfo.imageURL, contentMode: .scaleAspectFill)
+    cell.imageGame.image = UIImage(named: "star")
+    cell.imageGame.downloaded(from: appInfo.imageURL, contentMode: .scaleAspectFill, currentLine: indexPath.row)
     return cell
   }
   
@@ -68,7 +77,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
     let lastItem = elements.count - 1
     if indexPath.row == lastItem {
-      loadDada()
+      uploadData()
     }
   }
 }
--- CompletionProject/CompletionProject/Helpers/UIImageView+download.swift
+++ CompletionProject/CompletionProject/Helpers/UIImageView+download.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import ObjectiveC
 
 // FONTE: https://stackoverflow.com/questions/24231680/loading-downloading-image-from-url-on-swift
 
+private var associationKey: UInt8 = 0
+
 extension UIImageView {
-  func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+  
+  var currentLine: Int {
+    get {
+      return objc_getAssociatedObject(self, &associationKey) as! Int// & = endereço de memória da variável.
+    }
+    
+    set(newValue) {
+      objc_setAssociatedObject(self, &associationKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+  }
+  
+  func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit, currentLine: Int) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+    self.currentLine = currentLine
     contentMode = mode
     URLSession.shared.dataTask(with: url) { data, response, error in
       guard
@@ -20,14 +35,18 @@ extension UIImageView {
         let data = data, error == nil,
         let image = UIImage(data: data)
         else { return }
-      DispatchQueue.main.async() {
-        self.image = image
+      
+      if currentLine == self.currentLine{
+        DispatchQueue.main.async() {
+          self.image = image
+        }
       }
+      
       }.resume()
   }
   
-  func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+  func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit, currentLine: Int) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
     guard let url = URL(string: link) else { return }
-    downloaded(from: url, contentMode: mode)
+    downloaded(from: url, contentMode: mode, currentLine: currentLine)
   }
 }
